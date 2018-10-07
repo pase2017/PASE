4970871637001430935bd6024be4958140646b48
pase2017 <pase2017@163.com>
2018-10-07 20:17:54 +0800

    修改默认的 test/test_pase_matrix_vector/tool/build-linux.sh

--- test/test_pase_matrix_vector/tool/build-linux.sh
+++ test/test_pase_matrix_vector/tool/build-linux.sh
@@ -1,4 +1,4 @@
-cmake -Dcmake_module_root=/media/psf/Home/Workspace/pase-2.7/config \
-      -Dpase_root=/media/psf/Home/Workspace/pase-2.7/build-linux/pase \
+cmake -Dcmake_module_root=/media/psf/Home/Workspace/PASE/config \
+      -Dpase_root=/media/psf/Home/Workspace/PASE/build-linux/pase \
       -Duse_hypre=1 -Dhypre_root=/home/ycg/Software/hypre-2.11.2/src/hypre \
       ../
